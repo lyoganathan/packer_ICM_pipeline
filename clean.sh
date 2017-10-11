35a996a6159bceee69ace77ace130639e2529292
lyoganathan <yoganal@mcmaster.ca>
2017-10-10 23:38:04 -0400

    Cleanup some files
    remove installers

--- clean.sh
+++ clean.sh
@@ -8,7 +8,7 @@ echo "alias itksnap='/usr/local/itksnap-3.6.0-20170401-Linux-x86_64/bin/itksnap'
 
 #freesrufer and fsl setup
 echo "export FREESURFER_HOME=/usr/local/freesurfer" >> .bashrc
-echo "source $FREESURFER_HOME/SetUpFreeSurfer.sh" >> .bashrc
+echo "source \$FREESURFER_HOME/SetUpFreeSurfer.sh" >> .bashrc
 echo "source /etc/fsl/5.0/fsl.sh" >> .bashrc
 
 #Remove setup programs in /home/vagrant
