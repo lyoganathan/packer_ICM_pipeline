d77bf39cbb568051b61e7b78972af059b7862402
lyoganathan <yoganal@mcmaster.ca>
2017-10-16 16:39:42 -0400

    Start and Clean

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #bash aliases
-echo "alias dcm2nii='/home/vagrant/mricron_lx/dcm2nii'" > .bash_aliases
-echo "alias mricron='/home/vagrant/mricron_lx/mricron'" >> .bash_aliases
-echo "alias mipav='/home/vagrant/mipav/mipav'" >> .bash_aliases
+echo "alias dcm2nii='/home/ubuntu/mricron_lx/dcm2nii'" > .bash_aliases
+echo "alias mricron='/home/ubuntu/mricron_lx/mricron'" >> .bash_aliases
+echo "alias mipav='/home/ubuntu/ubuntu/mipav'" >> .bash_aliases
 echo "alias itksnap='/usr/local/itksnap-3.6.0-20170401-Linux-x86_64/bin/itksnap'" >> .bash_aliases
 
 #freesrufer and fsl setup
@@ -12,4 +12,5 @@ echo "source \$FREESURFER_HOME/SetUpFreeSurfer.sh" >> .bashrc
 echo "source /etc/fsl/5.0/fsl.sh" >> .bashrc
 
 #Remove setup programs in /home/vagrant
-rm /home/vagrant/*.gz
+rm /home/ubuntu/*.gz
+rm /home/ubuntu/*.zip
--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
-sudo apt-get install -y python3-pip
-pip3 install --upgrade pip
 sudo rm -rfv /var/lib/apt/lists/*
 sudo apt-get update
-while [ $? -ne 0 ]; do
-  sudo apt-get update
-done
+sudo apt-get install -y python3-pip
+pip3 install --upgrade pip
 sudo apt-get install -y python3-setuptools
 sudo pip3 install ansible
+pip3 install --upgrade cryptography
